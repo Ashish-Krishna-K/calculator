032afc251b20b6d0d5cd409ad219d907fc737b36
ashish-krishna-k <ashishk.krishna@gmail.com>
2023-07-26 21:48:38 +0530

    Creates the function that handles the operation.

--- script.ts
+++ script.ts
@@ -10,3 +10,22 @@ const divide = (a: number, b: number):DivisionError | number  => {
     return a / b;
 };
 
+// Create 4 variables to hold the firstNumber, secondNumber, operator and displayNumber
+let firstNumber = null;
+let secondNumber = null;
+let operator = null;
+let displayNumber: number | DivisionError = 0;
+
+// Create a function that does the calculation and returns the result
+const operate = (leftOperand: number, operator: string, rightOperand: number) => {
+    switch (operator) {
+        case ('+'):
+            return add(leftOperand, rightOperand);
+        case ('-'):
+            return subtract(leftOperand, rightOperand);
+        case ('*'):
+            return multiply(leftOperand, rightOperand);
+        case ('/'):
+            return divide(leftOperand, rightOperand);
+    }
+}
